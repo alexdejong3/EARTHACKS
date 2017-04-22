17f6c4e938171f04ded773956a9fa1ec5fd2e2c5
pewnocolypse <alqaisinisreen@gmail.com>
2017-04-22 00:25:49 -0600

    Nisreen's commit

--- app/src/main/java/com/earthhacks/hacks/MainActivity.java
+++ app/src/main/java/com/earthhacks/hacks/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //I was here :D
+        //Nisreen was....
     }
 }
