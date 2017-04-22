e45e9d1008c3dcc41838a704ca502998ca2cb07b
Alex D <alexdejong737@gmail.com>
2017-04-22 01:35:04 -0500

    added submissionactivity frame

---
+++ app/src/main/java/com/earthhacks/hacks/SubmissionActivity.java
@@ -0,0 +1,13 @@
+package com.earthhacks.hacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubmissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submission);
+    }
+}
