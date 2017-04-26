e403cb31902fe6e431b16de6a55d95976bb71104
Alex D <alexdejong737@gmail.com>
2017-04-25 19:13:29 -0500

    Cleaned up Data.java

--- app/src/main/java/com/earthhacks/hacks/Data.java
+++ app/src/main/java/com/earthhacks/hacks/Data.java
@@ -15,18 +15,34 @@ import java.util.Map;
 class Data {
 
     private static String data = "";
+
+    /*
+    Credentials that allow access to pastebin.com, the temporary server for this application
+     */
     private static final PastebinAccount ACCOUNT = new PastebinAccount("0c74dc3b025e92ece303a8d5ece9a0b9", "alipervaiz3", "M7kLm49M");
     private static final String urlpost = "http://pastebin.com/api/api_post.php";
     private static final String accountVerification = "5b0c7820f90de35c8ef6a361a697edf2";
 
+    /*
+    Returns the data in bit string
+                        zipcode
+                        date/timestamp format
+    else, returns an empty string
+     */
     public static String getData() {
         return data;
     }
 
-    static void retrieveData(){
+    /*
+    Calls to the server/pastebin website to get the data and save it
+     */
+    static void retrieveData() {
         getPaste();
     }
 
+    /*
+    Gets the first paste on the ACCOUNT
+     */
     private static void getPaste() {
         StringRequest pasteRequest = new StringRequest(Request.Method.POST, urlpost,
                 new Response.Listener<String>() {
@@ -37,11 +53,12 @@ class Data {
                         getDatafromPaste(pasteKey);
                     }
                 }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("ERROR", error.toString());
-            }
-        }) {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("ERROR", error.toString());
+                    }
+                })
+        {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
@@ -56,35 +73,44 @@ class Data {
         ApplicationController.getInstance().addToRequestQueue(pasteRequest);
     }
 
-    private static String getKeyFromData(String result){
+    /*
+    Parses paste data, returning the paste id used to get the actual content
+     */
+    private static String getKeyFromData(String result) {
         int index = result.indexOf("<paste_key>") + "<paste_key>".length();
         String s = "";
-        while(index < result.length() && result.charAt(index) != '<'){
-            s+=result.charAt(index++);
+        while (index < result.length() && result.charAt(index) != '<') {
+            s += result.charAt(index++);
         }
-        Log.d("PASTE_KEY",s);
+        Log.d("PASTE_KEY", s);
         return s;
     }
 
-    private static void getDatafromPaste(String pasteKey){
+    /*
+    Get the content of the paste to be used by other modules
+     */
+    private static void getDatafromPaste(String pasteKey) {
         StringRequest paste = new StringRequest(Request.Method.GET, "https://pastebin.com/raw/" + pasteKey,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.d("FINAL_DATA",response);
+                        Log.d("FINAL_DATA", response);
                         setData(response);
                     }
                 }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("ERROR", error.toString());
-            }
-        }
-        );
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("ERROR", error.toString());
+                    }
+                });
+
         ApplicationController.getInstance().addToRequestQueue(paste);
     }
 
-    static void setData(String response){
+    /*
+    Helper function to set the data from the interior getDatafromPaste function
+     */
+    static void setData(String response) {
         data = response;
     }
 }
